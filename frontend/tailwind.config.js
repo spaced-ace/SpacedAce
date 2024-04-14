8148e4222bc55d5ce36a92538eb409da6afcd733
Lényi Martin <lenyim00@gmail.com>
2024-04-14 17:40:22 +0200

    Add quiz fetching to my-quizzes page, and modify quiz creation page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/*.html"],
+  safelist: [
+    {
+      pattern: /from-(red|orange|amber|yellow|green|blue|purple|pink|lime|emerald|teal|cyan|indigo|violet|fuchsia|rose)-\d{3}/,
+    },
+    {
+      pattern: /to-(red|orange|amber|yellow|green|blue|purple|pink|lime|emerald|teal|cyan|indigo|violet|fuchsia|rose)-\d{3}/,
+    },
+  ],
   theme: {
-    extend: {},
   },
   plugins: [],
 }
