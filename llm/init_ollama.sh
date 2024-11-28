a4a35ada415f0d7a7ee3915e1a9ae80672a1a39f
jazzysnake <dotya77@gmail.com>
2024-11-28 17:59:46 +0100

    automated model creation from .gguf adapter

--- llm/init_ollama.sh
+++ llm/init_ollama.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <model-name> <path-to-adapter-gguf>"
+    exit 1
+fi
+
+model_name="$1"
+adapter_path="$2"
+
+if [ ! -f "$adapter_path" ]; then
+    echo "Error: Adapter not found at the specified path: $adapter_path"
+    exit 1
+fi
+
 # Get the IP address of the ollama container
 container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ollama)
 
@@ -9,16 +22,41 @@ if [ -z "$container_ip" ]; then
     exit 1
 fi
 
-# Generate the URL for the curl request
-url="http://$container_ip:11434/api/create"
+BASE_URL="http://$container_ip:11434/api"
 
-# Check if Modelfile exists in the current directory
-if [ ! -f "Modelfile" ]; then
-    echo "Error: Modelfile not found in the current directory."
+# Pull the model
+status_code=$(curl -s -o /dev/null -w "%{http_code}" "$BASE_URL/pull" -d '{"model": "llama3:8b-instruct-q4_0"}')
+if [ "$status_code" -eq 200 ]; then
+  echo "Downloaded llama model"
+else
+  echo "Request failed with status code: $status_code"
+fi
+
+# Get adapter checksum
+digest=$(sha256sum "$adapter_path" | cut -d' ' -f1)
+if [ -z "$digest" ]; then
+    echo "Error: Unable to retrieve digest of adapter"
     exit 1
 fi
 
-# Read the contents of Modelfile
-modelfile_content=$(sed 's/\\/\\\\/g; s/"/\\"/g; s/$/\\n/' Modelfile | tr -d '\n')
+# Upload adapter as blob
+status_code=$(curl -X POST -T "$adapter_path" -s -o /dev/null -w "%{http_code}" "$BASE_URL/blobs/sha256:$digest")
 
-curl -s -X POST "$url" -d '{"name": "mistral-quizgen", "modelfile": "'"$modelfile_content"'"}'
+if [ "$status_code" -eq 201 ] || [ "$status_code" -eq 200 ]; then
+  echo "Uploaded adapter to ollama"
+else
+  echo "Adapter upload request failed with status code: $status_code"
+  exit 1
+fi
+
+# Read the contents of Modelfile
+#modelfile_content=$(sed 's/\\/\\\\/g; s/"/\\"/g; s/$/\\n/' $"modelfile_path"| tr -d '\n')
+modelfile_content="FROM llama3:8b-instruct-q4_0\\nADAPTER /root/.ollama/models/blobs/sha256-$digest"
+
+status_code=$(curl -s -X POST -s -o /dev/null -w "%{http_code}" "$BASE_URL/create" -d '{"name": "'"$model_name"'", "modelfile": "'"$modelfile_content"'"}')
+if [ "$status_code" -eq 201 ] || [ "$status_code" -eq 200 ]; then
+  echo "Model creation successful"
+else
+  echo "Model creation request failed with status code: $status_code"
+  exit 1
+fi
